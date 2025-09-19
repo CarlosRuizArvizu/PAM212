3ac9cd66baeff49c500b54b22f91410156448cb1
CarlosRuizArvizu <124049028@upq.edu.mx>
2025-09-18 19:22:59 -0600

    Práctica No.1 Variables  & Funciones

---
+++ Practica1/Practica1_1.js
@@ -0,0 +1,15 @@
+// b. Convierte esta función tradicional a una arrow function que haga exactamente lo mismo:
+const cuadrado = (numero) => {
+    return numero * numero;
+};
+
+//console. log();
+//muestra en consola en probando con 3 numeros diferentes
+console.log(cuadrado(2));  
+console.log(cuadrado(5));  
+console.log(cuadrado(10)); 
+
+/* Crea una arrow function llamada saludoPersonalizado que reciba dos
+parámetros: nombre y edad, y retorne una cadena como la siguiente */
+
+// "Hola, me llamo Isay y tengo 37 anos."
---
+++ Practica1/Practica1_2.js
@@ -0,0 +1,4 @@
+const saludoPersonalizado = (nombre, edad) => "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+
+console.log(saludoPersonalizado("Isay", 37)); 
+console.log(saludoPersonalizado("Ana", 25));   
