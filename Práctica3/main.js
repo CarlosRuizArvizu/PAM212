4d4587e7f250cc91abe1a021b90495fd37c59261
CarlosRuizArvizu <124049028@upq.edu.mx>
2025-09-25 21:44:01 -0600

    Ejercicio A
    Se crea una función llamada restar que recibe dos parámetros: a y b. La función devuelve (return) el resultado de la resta a - b.

---
+++ Práctica3/main.js
@@ -0,0 +1,6 @@
+const restar = require("./utils");
+
+console.log("Pruebas de la función restar:");
+console.log("10 - 5 =", restar(10, 5));
+console.log("20 - 7 =", restar(20, 7));
+console.log("0 - 0 =", restar(0, 0));
